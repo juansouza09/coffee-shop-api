41b50084be521391c50243c3f89ddabb98e98cbe
juansouza09 <juansouza9.dev@gmail.com>
2024-12-06 21:57:49 -0300

    test added

--- src/main/java/com/jssolutions/coffee_shop_api/infrastructure/controllers/AddressController.java
+++ src/main/java/com/jssolutions/coffee_shop_api/infrastructure/controllers/AddressController.java
@@ -2,6 +2,7 @@ package com.jssolutions.coffee_shop_api.infrastructure.controllers;
 
 import com.jssolutions.coffee_shop_api.domain.Address;
 import com.jssolutions.coffee_shop_api.infrastructure.services.CepServiceImpl;
+import com.jssolutions.coffee_shop_api.infrastructure.services.LogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/address")
 public class AddressController {
+    private final LogService logService;
+    private final CepServiceImpl cepService;
+
     @Autowired
-    private CepServiceImpl cepService;
+    public AddressController(LogService logService, CepServiceImpl cepService) {
+        this.logService = logService;
+        this.cepService = cepService;
+    }
 
     @GetMapping("/{cep}")
     public Address getAddressByCep(@PathVariable String cep) {
-        return cepService.getAddressByCep(cep);
+        logService.logApiCall("Request: GET /api/address/" + cep, null);
+
+        Address address = cepService.getAddressByCep(cep);
+
+        logService.logApiCall(null, "Response: " + (address != null ? address.toString() : "Address not found"));
+
+        return address;
     }
 }
---
+++ src/test/java/com/jssolutions/coffee_shop_api/CepServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.jssolutions.coffee_shop_api;
+
+import com.jssolutions.coffee_shop_api.domain.Address;
+import com.jssolutions.coffee_shop_api.infrastructure.services.CepServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(properties = {"api.base.url=http://localhost:8081"})
+public class CepServiceImplTest {
+
+    @Autowired
+    private CepServiceImpl cepService;
+
+    @Test
+    public void testGetAddressByCep() {
+        Address address = cepService.getAddressByCep("05000-000");
+
+        assertNotNull(address);
+        assertEquals("05000-000", address.getCep());
+        assertEquals("Rua Teste", address.getLogradouro());
+        assertEquals("Bairro Teste", address.getBairro());
+        assertEquals("São Paulo", address.getLocalidade());
+        assertEquals("SP", address.getUf());
+    }
+}
